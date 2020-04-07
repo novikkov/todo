bf00073ab6cae538c8a90cd94dd1177f657ec13e
Yury Novikov <nov8861@yandex.ru>
2020-04-07 15:54:07 +0300

    create tasks

--- src/components/AddList/index.jsx
+++ src/components/AddList/index.jsx
@@ -6,9 +6,28 @@ import closeSvg from '../../assets/icons/close.svg'
 
 import './AddList.scss';
 
-const AddList = ({ colors }) => {
+const AddList = ({ colors, onAdd }) => {
   const [visiblePopup, setVisiblePopup] = useState(false);
   const [selectedColor, selectColor] = useState(colors[0].id);
+  const [inputValue, setInputValue] = useState('');
+
+  const onClose = () => {
+    setVisiblePopup(false);
+    setInputValue('');
+    selectColor(colors[0].id);
+  };
+
+  const addList = () => {
+    if(!inputValue) {
+      alert('Введите название списка');
+      return;
+    }
+    
+    const color = colors.filter(c => c.id === selectedColor)[0].name;
+
+    onAdd({ id: Math.random(), name: inputValue, color });
+    onClose();
+  }
 
     return (
       <div className="add-list">
@@ -44,12 +63,18 @@ const AddList = ({ colors }) => {
         {visiblePopup && (
           <div className="add-list__popup">
             <img 
-              onClick={() => setVisiblePopup(false)}
+              onClick={onClose}
               src={closeSvg} 
               className="add-list__popup-close" 
               alt="close" 
             />
-            <input className="field" type="text" placeholder="Название списка" />
+            <input 
+              value={inputValue} 
+              onChange={e => setInputValue(e.target.value)}
+              className="field" 
+              type="text" 
+              placeholder="Название списка" 
+            />
             <div className="add-list__popup-colors">
               {colors.map(color => (
                 <Badge
@@ -60,7 +85,7 @@ const AddList = ({ colors }) => {
                 />
               ))}
             </div>
-            <button className="button">Добавить</button>
+            <button onClick={addList} className="button">Добавить</button>
           </div>
         )}
       </div>
